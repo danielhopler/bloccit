574f81663403f168b05c8286bafa9211f191cce6
Daniel Hopler <hopler.daniel@gmail.com>
2018-05-07 16:53:07 -0400

    Add favorites and email notification

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -14,4 +14,17 @@ class FavoritesController < ApplicationController
     redirect_to [post.topic, post]
   end
 
+  def destroy
+    post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find(params[:id])
+
+    if favorite.destroy
+      flash[:notice] = "Post Unfavorited."
+    else
+      flash[:alert] = "Unfavoriting failed."
+    end
+
+    redirect_to [post.topic, post]
+  end
+
 end
